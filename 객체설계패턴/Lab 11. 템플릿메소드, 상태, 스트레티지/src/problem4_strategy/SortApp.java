0f42c703b29c32c28a0bf7c5d5c2cde375098cc5
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-05-26 17:48:05 +0900

    lab 11. problem4_strategy Sort finished

---
+++ 객체설계패턴/Lab 11. 템플릿메소드, 상태, 스트레티지/src/problem4_strategy/SortApp.java
@@ -0,0 +1,17 @@
+package problem4_strategy;
+
+public class SortApp {
+    public static void main(String[] args) {
+        SortedList studentRecords = new SortedList();
+        studentRecords.add("Samual");
+        studentRecords.add("Jimmy");
+        studentRecords.add("Sandra");
+//        studentRecords.setSortStrategy(new QuickSort());
+//        studentRecords.sort();
+        studentRecords.setSortStrategy(new ShellSort());
+        studentRecords.sort();
+//        studentRecords.setSortStrategy(new MergeSort());
+//        studentRecords.sort();
+
+    }
+}
---
+++ 객체설계패턴/Lab 11. 템플릿메소드, 상태, 스트레티지/src/problem4_strategy/SortStrategy.java
@@ -0,0 +1,10 @@
+package problem4_strategy;
+
+import java.util.List;
+
+public abstract class SortStrategy {
+    protected String[] arr;
+
+    public abstract List<String> sort(List<String> list);
+
+}
